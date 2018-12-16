2d98dd64b562f60f7fc2e1364e665153380786e4
Shalain DeRamus <shalainderamus@PSADMINs-MacBook-Air-6.local>
2018-12-16 07:42:53 -0500

    added modal to game question and added timer to close modal

--- gameboard.js
+++ gameboard.js
@@ -1,15 +1,54 @@
 
+// Get the modal
+var modal = document.getElementById('quesModal');
+
+// Get the button that opens the modal
+//change this to each ques button 
+// var quesBtn = document.getElementById("quesBtn");
+
+var quesBtn = document.getElementsByClassName("gameRow")[1];
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+quesBtn.onclick = function() {
+  modal.style.display = "block";
+  //set timer to close modal when time runs out 
+  setTimeout(function(){ modal.style.display = "none"; }, 3000);
+
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+//class contructor will get given the following arguments
+//question with answer choices and correct answer
+//position of question on gameboard using class index 
 class questions {
-    constructor(question, answers, points, rightanswer){
+    constructor(question, answers, points, rightanswer, index){
         this.question = question
         this.answers = answers
         this.rightanswer = rightanswer
         this.points = points
+        this.index = index
     }
 
     showQuestion(){
         //show users pop out modal 
+        quesBtn.onclick 
         //display question and answers 
+        
         //grab users answer
         //if time runs out, say times up and close modal, subtract points
     }
@@ -29,32 +68,6 @@ class questions {
 
 }
 
-
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
 
 
